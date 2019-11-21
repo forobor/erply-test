09a1465a11baa59087d4745feeb34ea41f1ed179
Demid Ost <demostapenko@gmail.com>
2019-11-21 17:12:41 +0200

    redirect, readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch } from "react-router-dom";
+import { Switch, Redirect } from "react-router-dom";
 import { LoginPage, MainPage, ArticlePage, ProfilePage } from "./pages";
 import { Header, MessagePopup, ProtectedRoute, Loading } from "./components";
 import AuthContextProvider from "./state/contexts/AuthContext";
@@ -18,6 +18,7 @@ const App: React.FC = () => {
 						<ProtectedRoute path="/main" component={MainPage} />
 						<ProtectedRoute path="/article" component={ArticlePage} />
 						<ProtectedRoute path="/profile" component={ProfilePage} />
+						<Redirect to="/" />
 					</Switch>
 				</React.Fragment>
 			</NewsContextProvider>
